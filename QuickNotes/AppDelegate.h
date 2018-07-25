77b72a37aeb7c8fc0bb4fc737b723a1fe8d9227a
Sergey Krotkih <sergey.krotkih@gmail.com>
2018-07-25 15:47:06 +0300

    create Xcode project. make first commit.

---
+++ QuickNotes/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  QuickNotes
+//
+//  Created by Sergey Krotkih on 25/07/2018.
+//  Copyright © 2018 Sergey Krotkih. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ QuickNotes/Scenes/Main/QNMainViewController.m
@@ -0,0 +1,29 @@
+//
+//  QNMainViewController.m
+//  QuickNotes
+//
+//  Created by Sergey Krotkih on 25/07/2018.
+//  Copyright © 2018 Sergey Krotkih. All rights reserved.
+//
+
+#import "QNMainViewController.h"
+
+@interface QNMainViewController ()
+
+@end
+
+@implementation QNMainViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
